610bf0094af6555d189d4dc940f4bf12ffd8fe22
PedroHenriqMra <pedrohenriquemraa@gmail.com>
2026-01-14 23:37:21 -0300

    add client input gameplay/admin

--- Src/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Src/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883d9454adfa92cec48ae8cb7562bb89250696f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+873bd563adcc1f6786286dd6824162805c27fdf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
